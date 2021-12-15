1af98e2238fe7d406fba760cb42ccdc59465079f
Jorge Minjares <jorge.minjaresps3@gmail.com>
2021-12-15 16:10:42 -0700

    Created a blinking LED example

---
+++ Blinky LEDS/main.c
@@ -0,0 +1,22 @@
+#include "msp.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	P1->DIR |= BIT0;
+	P1->OUT &= ~(BIT0);
+
+
+
+	while(1){
+	    P1->OUT = BIT0;
+	    __delay_cycles(1000000);
+	    P1->OUT &= ~(BIT0);
+	    __delay_cycles(1000000);
+	}
+}
