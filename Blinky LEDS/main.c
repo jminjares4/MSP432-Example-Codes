f8cb1b2194fcab7eaea3c7d336a950087e4cad1d
jminjares4 <jminjares4@miners.utep.edu>
2021-12-15 18:06:26 -0700

    Updated main.c

--- Blinky LEDS/main.c
+++ Blinky LEDS/main.c
@@ -1,19 +1,37 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief This program will toggle various onboard leds with the intention of
+ * 		  demostrating how to use OUTPUTs.
+ * @version 0.1
+ * @date 2021-12-15
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #include "msp.h"
 
-/**
- * main.c
- */
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
 
+	// Initialze P1.0 as output
 	P1->DIR |= BIT0;
-	P1->OUT &= ~(BIT0);
+	// clear BIT0
 
-	while(1){
-	    P1->OUT = BIT0;
-	    __delay_cycles(500000);
-	    P1->OUT &= ~(BIT0);
-	    __delay_cycles(500000);
+	P1->OUT &= ~(BIT0);
+	// Clear secondary functions
+
+	P1->SEL1 &= ~(BIT0);
+	P1->SEL2 &= ~(BIT0);
+
+	while (1)
+	{
+		P1->OUT = BIT0; // set BIT0 as high!
+		__delay_cycles(500000);
+		P1->OUT &= ~(BIT0);
+		__delay_cycles(500000); // set BIT0 as low!
 	}
 }
--- Hello World/main.c
+++ Hello World/main.c
@@ -1,30 +1,32 @@
 /**
  * @file main.c
- * @author Jesus Minjares (https://github.com/jminjares4) 
+ * @author Jesus Minjares (https://github.com/jminjares4)
  * @author Jorge Minjares (https://github.com/JorgeMinjares)
  * @brief This is a simple hello world! Typically 'Hello World' is use when starting any
  * 		  programming language. However, in embedded we turn an LED!
  * @version 0.1
  * @date 2021-12-15
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 #include "msp.h"
+
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
 
-	//Initialze P1.0 as output
-	P1->DIR |= BIT0; 
-	//clear BIT0
-	P1->OUT &= ~(BIT0); 
-	//Clear secondary functions
+	// Initialze P1.0 as output
+	P1->DIR |= BIT0;
+	// clear BIT0
+	P1->OUT &= ~(BIT0);
+	// Clear secondary functions
 	P1->SEL0 &= ~(BIT0);
 	P1->SEL1 &= ~(BIT0);
 
-	while(1){ //infinite loop, to avoid WDT
-		P1->OUT = BIT0; //set BIT0 as high!
-		__delay_cycles(1000000); //add a delay in MCU
+	while (1)
+	{							 // infinite loop, to avoid WDT
+		P1->OUT = BIT0;			 // set BIT0 as high!
+		__delay_cycles(1000000); // add a delay in MCU
 	}
 }
