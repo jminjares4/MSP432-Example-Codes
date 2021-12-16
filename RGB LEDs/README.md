3d6da06a6fcccb1ba7b4272a1e6e05948fae6d9d
jminjares4 <jminjares4@miners.utep.edu>
2021-12-15 19:37:46 -0700

    Updated file

---
+++ RGB LEDs/README.md
@@ -0,0 +1,15 @@
+# **RGB LEDs :zap:**
+
+## Description
+This code toggles use all the possible comnbinations of hte `on-board LEDs` to generate various colors. All the combination
+will be store in MACROs and then create an global array to iterate over to display the different LEDs colors. 
+
+## Pinout
+![image](https://user-images.githubusercontent.com/60948298/146273491-d2079ae0-385a-4f9a-ac03-24f95911efea.png)
+    - Texas Instrument
+    - 
+## Authors:
+  - Jesus Minjares :zap:
+    - Master of Science in Computer Engineering
+  - Jorge Minjares :zap:
+    - Bachelor of Science in Electrical Engineering
--- RGB LEDs/main.c
+++ RGB LEDs/main.c
@@ -1,9 +1,17 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief This code toggles use all the possible comnbinations of hte `on-board LEDs` to generate various colors.
+ * @version 0.1
+ * @date 2021-12-15
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include "msp.h"
 
-
-/**
- * main.c
- */
+//Combination of MACROs for the onboard leds
 #define OFF 0x00
 #define RED 0x01
 #define GREEN 0x02
@@ -12,37 +20,31 @@
 #define PURPLE 0x05
 #define TURQUOISE 0x06
 #define WHITE 0x07
-//
-//typedef enum{
-//OFF = 0x00,
-//RED = 0x01,
-//GREEN = 0x02,
-//YELLOW = 0x03,
-//BLUE = 0x04,
-//PURPLE = 0x05,
-//TURQUOISE = 0x06,
-//WHITE = 0x07,
-//}color_t;
 
-int colors[] = {OFF,RED, GREEN, YELLOW, BLUE, PURPLE, TURQUOISE, WHITE};
+//Global array
+int colors[] = {OFF, RED, GREEN, YELLOW, BLUE, PURPLE, TURQUOISE, WHITE};
 
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
 
-//	color_t color = OFF;
-	P2->DIR |= BIT0|BIT1|BIT2; //all outputs
+    //Set all onboard LEDs as outputs
+    P2->DIR |= BIT0 | BIT1 | BIT2; // all outputs
+    //Clear all onbaard LEDs 
+    P2->OUT &= ~(BIT0 | BIT1 | BIT2);
+    //Clear all secondary funcitons pins
+    P2->SEL1 &= ~(BIT0 | BIT1 | BIT2);
+    P2->SEL2 &= ~(BIT0 | BIT1 | BIT2);
 
-	P2->OUT &= ~(BIT0|BIT1|BIT2);
-    while(1){
-        int i;
-        for(i = 0; i < 8; i++){
-            P2->OUT = colors[i];
-            __delay_cycles(500000);
+    while (1) //all embedded system must have an infinite loop
+    {
+        int i; //create an variable name i 
+        //iterate over the length of the colors array
+        for (i = 0; i < sizeof(colors) / sizeof(int); i++)
+        {
+            P2->OUT = colors[i]; //update OUTPUT with the array index 
+            __delay_cycles(500000); //small delay to be able to visualize the changes 
         }
-//            color = (color_t)i;
-//            P2->OUT = color;
-//            __delay_cycles(1000000);
-//        }
+        __delay_cycles(500000); //small delay
     }
 }
