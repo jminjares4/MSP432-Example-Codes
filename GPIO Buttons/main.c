d36d35e088e531a08dba990eb2e853a1d415dcc5
jminjares4 <jminjares4@miners.utep.edu>
2021-12-16 15:44:57 -0700

    Updated main.c

--- GPIO Buttons/main.c
+++ GPIO Buttons/main.c
@@ -1,12 +1,33 @@
 #include "msp.h"
-
-
-/**
- * main.c
- */
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
+	//output setup
+	P1->DIR |= BIT0;
+	P1->OUT &= ~(BIT0);
+	P1->SEL0 &= ~(BIT0);
+	P1->SEL1 &= ~(BIT0);
 
+	//pull-down setup
+	P6->DIR &= ~(BIT0);
+	P6->OUT &= ~(BIT0);
+	P6->REN |= BIT0;
+
+	//pull-up setup
+	P4->DIR &= ~(BIT0);
+	P4->OUT |= BIT0;
+	P4->REN |= BIT0;
+
+	while(1){
+		if((P4->IN & BIT0) == 0x00){ // 1 -> 0
+			P1->OUT = BIT0;	
+		}else if((P6->IN & BIT0) == BIT0){ // 0 - >
+			P1->OUT &= ~(BIT0);
+		}else{
+			__delay_cycles(10000); //small delay 
+		}
+		//debounce the button
+		__delay_cycles(1000000);
+	}
 }
