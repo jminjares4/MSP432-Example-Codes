bb7aa23fd64978c926b1a4cd35cf9c636d89107b
jminjares4 <jminjares4@miners.utep.edu>
2021-12-18 13:01:42 -0700

    Updated main.c

--- GPIO Buttons/main.c
+++ GPIO Buttons/main.c
@@ -1,3 +1,14 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief This is a simple input code to demostrate how to create pull-up or pull-down buttons.
+ * @version 0.1
+ * @date 2021-12-15
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include "msp.h"
 void main(void)
 {
@@ -11,17 +22,18 @@ void main(void)
 
 	//pull-down setup
 	P6->DIR &= ~(BIT0);
-	P6->OUT &= ~(BIT0);
+	P6->OUT &= ~(BIT0); // for pull down, OUT register must be cleared 
 	P6->SEL0 &= ~(BIT0);
 	P6->SEL1 &= ~(BIT0);
-	P6->REN |= BIT0;
+	P6->REN |= BIT0; //enable resistor
 
 	//pull-up setup
 	P4->DIR &= ~(BIT0);
-	P4->OUT |= BIT0;
+	P4->OUT |= BIT0; // for pull up, OUT register must be set as HIGH!
 	P4->SEL0 &= ~(BIT0);
 	P4->SEL1 &= ~(BIT0);
-	P4->REN |= BIT0;
+	P4->REN |= BIT0; //enable resistor
+
 	//infinite loop
 	while(1){ 
 		if((P4->IN & BIT0) == 0x00){ // 1 -> 0
