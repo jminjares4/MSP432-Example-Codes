eda62a792eefbbf4356b3a86772ebbf7894ad1a5
jminjares4 <jminjares4@miners.utep.edu>
2021-12-20 10:21:55 -0700

    Updated main.c and added README.md

---
+++ LED with Interupts/README.md
@@ -0,0 +1,47 @@
+# **LED with Interrupts :zap:**
+
+## Description
+This program will demostrate how to use GPIO inputs to select different led colors using interrupts. This example uses `pull-down` configuration!
+
+## Pinout
+![image](https://user-images.githubusercontent.com/60948298/146273491-d2079ae0-385a-4f9a-ac03-24f95911efea.png)
+- Texas Instrument
+
+## MSP432 I/O Configuration Table 
+| PxDIR | PxREN | PxOUT | I/O Configuration|
+| :---: | :---: | :---: |     :---         |     
+| 0     | 0     |  x    |  Input           |
+| 0     | 1     |  0    |  Input with pulldown resistor  |
+| 0     | 1     |  1    |  Input with pullup resistor |
+| 1     | x     |  x    |  Output         |
+  * I/O Configuration
+
+## MPS432 Interruot Configuration Table
+| PxIE  | PxIES | PxIFG | I/O Configuration  |
+| :---: | :---: | :---: |     :---           |     
+| 1     | 0     |  0    |  Pull-down         |
+| 1     | 1     |  0    |  Pull-up           |
+| x     | x     |  1    | Interrupt occured! |
+  * Interrupt Configuration
+
+## LEDs 
+| Color | Voltage (V) | Current (mA) |
+|:---   | :---:       |     ---:     |
+| Red   | 1.8-2.4     |      20      |
+| Yellow| 2.0-2.2     |      20      |
+| Green | 3.0-3.4     |      20      |
+| Blue  | 3.0-3.4     |      20      | 
+| White | 3.0-3.4     |      20      |
+  * Table 1 Typical LED with voltage and current requirements
+## LEDs Circuit
+<img src="https://user-images.githubusercontent.com/60948298/146442932-b87b92c4-9241-40eb-b26e-057f261b8fbc.png" width="450" height="250">
+
+## **Pull-up and Pull-down Configuration**
+
+<img width="413" alt="button configuation" src="https://user-images.githubusercontent.com/60948298/144836131-96f04e0f-c7f7-443f-b35c-814fb9db4e29.png">
+
+## Authors:
+  - Jesus Minjares :zap:
+    - Master of Science in Computer Engineering
+  - Jorge Minjares :zap:
+    - Bachelor of Science in Electrical Engineering
--- LED with Interupts/main.c
+++ LED with Interupts/main.c
@@ -1,52 +1,67 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief This program will demostrate how to use external GPIOs as interrupts
+ * @version 0.1
+ * @date 2021-12-15
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include "msp.h"
 
-
-/**
- * main.c
- */
-#define RED 0x01
-#define GREEN 0x02
-#define BLUE 0x04
+// MACROs for all onboard leds
+#define RED   0x01   // BIT0
+#define GREEN 0x02 // BIT1
+#define BLUE  0x04  // BIT2
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
 
-	//Set RGB LEDS
-	P2->DIR |= (0x07); //Bit Set:Port 2: BIT0|BIT1|BIT2
-	P2->OUT &= ~(0x07);//Bit clear Port 2
-	P2->SEL0 &= ~(0x07);//Bit Clear SEL0
-	P2->SEL1 &= ~(0x07);//Bit Clear SEl1
+	// Set RGB LEDS
+	P2->DIR |= (0x07);	 // Bit Set:Port 2: BIT0|BIT1|BIT2
+	P2->OUT &= ~(0x07);	 // Bit clear Port 2
+	P2->SEL0 &= ~(0x07); // Bit Clear SEL0
+	P2->SEL1 &= ~(0x07); // Bit Clear SEl1
 
-	//Set Interupts using pull down setup: 0->1
-	P4->DIR &= ~(0x0F);//Bit set: Port 4: Bit0|BIT1|BIT2|BIT3
-	P4->OUT &= ~(0x0F);//Bit clear port 4
-	P4->REN |= 0x0F; //Activate internal resistor for port 4, bit: 0,1,2,3
-	P4->SEL0 &= ~(0x0F);//Bit clear SEL0
-	P4->SEL1 &= ~(0x0F);//bit clear SEL1
+	// Set Interupts using pull down setup: 0->1
+	P4->DIR &= ~(0x0F);	 // Bit set: Port 4: Bit0|BIT1|BIT2|BIT3
+	P4->OUT &= ~(0x0F);	 // Bit clear port 4
+	P4->REN |= 0x0F;	 // Activate internal resistor for port 4, bit: 0,1,2,3
+	P4->SEL0 &= ~(0x0F); // Bit clear SEL0
+	P4->SEL1 &= ~(0x0F); // bit clear SEL1
 
-	P4->IE |= 0x0F; //Enable interrupts for bit 0,1,2,3
-	P4->IES &= ~(0x0F);//Bit clear for pull-down
-	P4->IFG &= ~(0x0F); //Enable flag for interrupts
+	P4->IE |= 0x0F;		// Enable interrupts for bit 0,1,2,3
+	P4->IES &= ~(0x0F); // Bit clear for pull-down
+	P4->IFG &= ~(0x0F); // Enable flag for interrupts
 
-	NVIC->ISER[1] = 1 << ((PORT4_IRQn) & 31);
+	//enable NVIC for port 4
+	NVIC->ISER[1] = 1 << ((PORT4_IRQn)&31); //see NVIC table, all PORTx go in ISER[1]
 
-	__enable_irq();
-	while(1){}
+	__enable_irq(); // enable global interrupts
+	while (1)//empty loop as every will be control by the Port Interrupt
+	{
+	}
 }
 
-void PORT4_IRQHandler(void){
-    if(P4->IFG & BIT0){
-        P2->OUT &= ~(0x0F);
-    }
-    if(P4->IFG & BIT1){
-        P2->OUT |= RED;
-    }
-    if(P4->IFG & BIT2){
-        P2->OUT |= GREEN;
-    }
-    if(P4->IFG & BIT3){
-        P2->OUT |= BLUE;
-    }
-    P4->IFG &= ~(0x0F);
+void PORT4_IRQHandler(void)
+{
+	if (P4->IFG & BIT0)
+	{						// check bit 0
+		P2->OUT &= ~(0x0F); // clear all bits
+	}
+	if (P4->IFG & BIT1)
+	{					// check bit 1
+		P2->OUT |= RED; // set RED led
+	}
+	if (P4->IFG & BIT2)
+	{					  // check bit 2
+		P2->OUT |= GREEN; // set GREEN led
+	}
+	if (P4->IFG & BIT3)
+	{					 // check bit 3
+		P2->OUT |= BLUE; // set BLUE led
+	}
+	P4->IFG &= ~(0x0F); // clear all bits on the interrupt flag
 }
-
