f50222d643306b13de1b08ed85926e360f591076
jminjares4 <jminjares4@miners.utep.edu>
2021-12-20 11:22:53 -0700

    Updated readme and main.c

--- Interrupt With Buttons/main.c
+++ Interrupt With Buttons/main.c
@@ -1,53 +1,67 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief This program will demostrate how to use interrupts
+ * @version 0.1
+ * @date 2021-12-20
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include "msp.h"
 
-
-/**
- * main.c
- */
-#define BLUE 0x04
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-	P1->DIR |= 0x01;
-	P1->OUT &= ~(0x01);
-	P1->SEL0 &= ~(0x01);
-	P1->SEL1 &= ~(0x01);
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
 
-	P2->DIR |= BLUE;
-	P2->OUT &= ~(BLUE);
-	P2->SEL0 &= ~(BLUE);
-	P2->SEL1 &= ~(BLUE);
+	// Output
+	P1->DIR |= BIT0;	// set BIT0 as OUTPUT
+	P1->OUT &= ~(BIT0); // set BIT0 as LOW
+	// clear secondary functions
+	P1->SEL0 &= ~(BIT0);
+	P1->SEL1 &= ~(BIT0);
+	// Output
+	P2->DIR |= BIT2;	// set BIT2 as OUTPUT
+	P2->OUT &= ~(BIT2); // set BIT2 as LOW
+	// clear secondary functions
+	P2->SEL0 &= ~(BIT2);
+	P2->SEL1 &= ~(BIT2);
 
-	//pull down, normal button setup
-	P4->DIR &= ~(BIT0|BIT1);
-	P4->OUT &= ~(BIT0|BIT1);
-	P4->SEL0 &= ~(BIT0|BIT1);
-	P4->SEL1 &= ~(BIT0|BIT1);
-	P4->REN |= BIT0|BIT1;
-	//intialize interrupt set up
-	P4->IE |= BIT0|BIT1; //enable bit0 as interrupt
-	P4->IES &= ~(BIT0|BIT1);
-	P4->IFG &= ~(BIT0|BIT1); //clear interrupt flag
+	// pull down, normal button setup
+	P4->DIR &= ~(BIT0 | BIT1); // set BIT0 and BIT1 as inputs
+	P4->OUT &= ~(BIT0 | BIT1); // set BIT0 and BIT1 as LOW for pull-down configuration
+	P4->REN |= BIT0 | BIT1;	   // enable resistors
+	// clear secondary functions
+	P4->SEL0 &= ~(BIT0 | BIT1);
+	P4->SEL1 &= ~(BIT0 | BIT1);
+	// Interrupt Configuration
+	P4->IE |= BIT0 | BIT1;	   // enable BIT0 and BIT1 as interrupts
+	P4->IES &= ~(BIT0 | BIT1); // set BIT0 and BIT1 as rising edge, 0->1 or pull-down
+	P4->IFG &= ~(BIT0 | BIT1); // clear interrupt flag
 
-	NVIC->ISER[1] = 1 << ((PORT4_IRQn) & 31); // set NVIC
+	// enable NVIC for Port 4
+	NVIC->ISER[1] = 1 << ((PORT4_IRQn)&31);
 
-	__enable_irq(); //enables the global interrupts
+	// enables the global interrupts
+	__enable_irq();
 
-    while(1){
-        P1->OUT ^= 0x01;
-        __delay_cycles(500000);
-    }
+	while (1)
+	{
+		P1->OUT ^= BIT0; //toggle the BIT0
+		__delay_cycles(500000); //delay to see the changes
+	}
 }
 
-// ISR!!!
-void PORT4_IRQHandler(void){
-    if(P4->IFG & BIT0){
-        P2->OUT |= BLUE;
-    }
-    if(P4->IFG & BIT1){
-        P2->OUT &= ~(BLUE);
-    }
-
-    P4->IFG &= ~(BIT0|BIT1);
+void PORT4_IRQHandler(void)
+{
+	if (P4->IFG & BIT0) //check if BIT0 was trigger
+	{
+		P2->OUT |= BIT2; //set BIT2 on
+	}
+	if (P4->IFG & BIT1) //check if BIT1 was trigger
+	{
+		P2->OUT &= ~(BIT2); //clear BIT2
+	}
+	P4->IFG &= ~(BIT0|BIT1); //clear interrupts flags
 }
-
