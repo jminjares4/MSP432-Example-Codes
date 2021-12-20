db59ee21bd82cab610b9cdaad135c6b3554093b5
jminjares4 <jminjares4@miners.utep.edu>
2021-12-20 11:01:36 -0700

    Updated readme and main.c

---
+++ Systick Timer/README.md
@@ -0,0 +1,30 @@
+# **SysTick Timer :zap:**
+
+## Description
+This program will demostrate how to use SysTick to create periodic tasks. The following code will toggle
+the onboard led every 5 seconds.
+
+## Pinout
+![image](https://user-images.githubusercontent.com/60948298/146273491-d2079ae0-385a-4f9a-ac03-24f95911efea.png)
+- Texas Instrument
+
+## MPS432 SysTick Configuration Table
+| Bit  | Name              | Descriptions |
+| :---: | :---:            | :--- |        
+| 0     | ENABLE           | Enable <br> 0: the counter is disabled<br> 1:enables SysTick to begin counting down|  
+| 1     | TICKINT          |  Interrupt Enable <br> 0: Interrupt generation is disabled <br>1: when SysTick counts to 0 an interrupt is generated<br>    | 
+| 2     | CLKSOURCE        |  Clock Source <br> 0: 0: External clock <br> 1: System clock    |
+  * SysTick->CTRL register table 
+
+Example of how to set the SysTick Timer. The following code will set the interrupt to run every second.
+~~~c
+SysTick->CTRL |= 0x07; //enable, tick int, and clock source
+SysTick->VAL = 0; //clear value register
+SysTick->LOAD  = 3000000 - 1; // 1 sec period, N - 1, load is 24 bit register->max = 0xFFFFFF 
+~~~
+
+## Authors:
+  - Jesus Minjares :zap:
+    - Master of Science in Computer Engineering
+  - Jorge Minjares :zap:
+    - Bachelor of Science in Electrical Engineering
--- Systick Timer/main.c
+++ Systick Timer/main.c
@@ -1,35 +1,59 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief Demostrate how to use SysTick to create periodic tasks
+ * @version 0.1
+ * @date 2021-12-20
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include "msp.h"
 
-uint8_t tic = 0;
+// global variable
+uint8_t tic = 0; // will control the total cpu clock cylces
 
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
 
-	P1->DIR |= BIT0;
-	P1->OUT &= ~(BIT0);
+	// Output
+	P1->DIR |= BIT0;	// set BIT0 as OUTPUT
+	P1->OUT &= ~(BIT0); // clear BIT0 to LOW
+	// clear secondary flags
 	P1->SEL0 &= ~(BIT0);
 	P1->SEL1 &= ~(BIT0);
 
-	P2->DIR |= BIT2;
-	P2->OUT &= ~(BIT2);
+	// Output
+	P2->DIR |= BIT2;	// set BIT2 as OUTPUT
+	P2->OUT &= ~(BIT2); // clear BIT2 to LOW
+	// clear secondary flags
 	P2->SEL0 &= ~(BIT2);
 	P2->SEL1 &= ~(BIT2);
 
-	SysTick->CTRL = 0x07; //start timer
-	SysTick->VAL = 1 - 1;
-	SysTick->LOAD = 300000; //100ms
-
+	// SysTick Configuration
+	// CLKSRC|TICKINT|ENABLE
+	SysTick->CTRL = 0x07;	// 1       1       1    ->  0x07, start timer
+	SysTick->VAL = 1 - 1;	// set 0 to the value
+	SysTick->LOAD = 300000; // 300000/3MHz - > 0.1s -> 100ms
+	
+	// enable global interrupts
 	__enable_irq();
-	while(1){
-	    P2->OUT ^= BIT2;
-	    __delay_cycles(5000000);
+
+	while (1)
+	{
+		P2->OUT ^= BIT2;
+		__delay_cycles(5000000);
 	}
 }
 
-void SysTick_Handler(void){
-    if(tic++ > 5*10){ //toggle every 5 seconds
-        P1->OUT ^= BIT0;
-        tic = 0;
-    }
+void SysTick_Handler(void)
+{
+	// every tic is 100ms therefore, 50 * 100ms -> 5 secs 
+	if (tic++ > 5 * 10) //check if 5 sec have passed
+	{ 
+		P1->OUT ^= BIT0; //toggle led 
+		tic = 0; //clear tic 
+	}
 }
