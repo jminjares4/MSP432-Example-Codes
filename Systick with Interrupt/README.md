8546cfe4e3715b6dcb63ad7d3607c376f00b028e
jminjares4 <jminjares4@miners.utep.edu>
2021-12-20 11:13:46 -0700

    Updated readme and main.c

---
+++ Systick with Interrupt/README.md
@@ -0,0 +1,51 @@
+# **SysTick Timer with Interrupts :zap:**
+
+## Description
+This program will demostrate how to use SysTick to create periodic tasks. The SysTick will be controlled by interrupts as they will disable or enable the timer.
+
+## Pinout
+![image](https://user-images.githubusercontent.com/60948298/146273491-d2079ae0-385a-4f9a-ac03-24f95911efea.png)
+- Texas Instrument
+
+## MSP432 I/O Configuration Table 
+| PxDIR | PxREN | PxOUT | I/O Configuration|
+| :---: | :---: | :---: |     :---         |     
+| 0     | 0     |  x    |  Input           |
+| 0     | 1     |  0    |  Input with pulldown resistor  |
+| 0     | 1     |  1    |  Input with pullup resistor |
+| 1     | x     |  x    |  Output         |
+  * I/O Configuration
+
+## MPS432 Interrupt Configuration Table
+| PxIE  | PxIES | PxIFG | I/O Configuration  |
+| :---: | :---: | :---: |     :---           |     
+| 1     | 0     |  0    |  Pull-down         |
+| 1     | 1     |  0    |  Pull-up           |
+| x     | x     |  1    | Interrupt occured! |
+  * Interrupt Configuration
+
+## **Pull-up and Pull-down Configuration**
+
+<img width="413" alt="button configuation" src="https://user-images.githubusercontent.com/60948298/144836131-96f04e0f-c7f7-443f-b35c-814fb9db4e29.png">
+
+
+## MPS432 SysTick Configuration Table
+| Bit  | Name              | Descriptions |
+| :---: | :---:            | :--- |        
+| 0     | ENABLE           | Enable <br> 0: the counter is disabled<br> 1:enables SysTick to begin counting down|  
+| 1     | TICKINT          |  Interrupt Enable <br> 0: Interrupt generation is disabled <br>1: when SysTick counts to 0 an interrupt is generated<br>    | 
+| 2     | CLKSOURCE        |  Clock Source <br> 0: 0: External clock <br> 1: System clock    |
+  * SysTick->CTRL register table 
+
+Example of how to set the SysTick Timer. The following code will set the interrupt to run every second.
+~~~c
+SysTick->CTRL |= 0x07; //enable, tick int, and clock source
+SysTick->VAL = 0; //clear value register
+SysTick->LOAD  = 3000000 - 1; // 1 sec period, N - 1, load is 24 bit register->max = 0xFFFFFF 
+~~~
+
+## Authors:
+  - Jesus Minjares :zap:
+    - Master of Science in Computer Engineering
+  - Jorge Minjares :zap:
+    - Bachelor of Science in Electrical Engineering
--- Systick with Interrupt/main.c
+++ Systick with Interrupt/main.c
@@ -1,66 +1,73 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief Demostrate how to use SysTick to create periodic tasks and control the SysTick with
+ * 		  interrupts. 
+ * @version 0.1
+ * @date 2021-12-20
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */ 
 #include "msp.h"
 
-
-/**
- * main.c
- */
-
-//toggle every 3 seconds
+//global variable 
 uint8_t tic = 0;
+
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-	//Enable on-board LED
-	P2->DIR |= BIT2;
-	P2->OUT &= ~(BIT2);
-	P2->SEL0 &= ~(BIT2);
+	//Output
+	P2->DIR |= BIT2; // set BIT2 as OUTPUT
+	P2->OUT &= ~(BIT2); // set BIT2 as LOW
+	//clear secondary functions
+	P2->SEL0 &= ~(BIT2); 
 	P2->SEL1 &= ~(BIT2);
 
-	//Pull down set up using Port 4 bit 0
-	P4->DIR &= ~(BIT0|BIT1);
-	P4->OUT &= ~(BIT0);
-	P4->REN |= BIT0|BIT1;
-	//Pull up set up using port 4 bit 1
-	P4->OUT |= BIT1;
+	//Inputs
+	P4->DIR &= ~(BIT0|BIT1); //set BIT0 and BIT1 as inputs
+	P4->OUT &= ~(BIT0); //set BIT0 as Pull-down
+	P4->OUT |= BIT1; //Pull up set up using port 4 bit 1
+	P4->REN |= BIT0|BIT1; //enable resistors
 	//Bit clear sel0 and sel1 for port 4
 	P4->SEL0 &= ~(BIT0|BIT1);
 	P4->SEL1 &= ~(BIT0|BIT1);
-	//enable interrupts for port 4
-	P4->IE |= BIT0|BIT1;
-	//set bit 0 IES as rising edge
-	P4->IES &= ~(BIT0);
-	//set bit 1 IES as falling edge
-	P4->IES |= BIT1;
-	//set Interrupt flag as 0
-	P4->IFG &= ~(BIT0|BIT1);
-	//SysTick bit 0: Enable SysTick and bit 2: Clock Source --
-	//BIT0|BIT2 = 0x05
-	SysTick->CTRL = 0x05;
-	SysTick->VAL = 1 - 1;
-	SysTick->LOAD = 300000;
+	//Interrupt Configuration
+	P4->IE |= BIT0|BIT1; //enable BIT0 and BIT1
+	P4->IES &= ~(BIT0); //set bit 0 IES as rising edge
+	P4->IES |= BIT1; //set bit 1 IES as falling edge
+	P4->IFG &= ~(BIT0|BIT1); //set Interrupt flag as 0
+
+	//SysTick Configuration
+						   // CLKSRC|TICKINT|ENABLE
+	SysTick->CTRL = 0x05; //   1       0       1    ->  0x05, disable interrupt
+	SysTick->VAL = 1 - 1; //
+	SysTick->LOAD = 300000 - 1; // 30000/3Mhz -> .1sec -> 100ms
 
 	//Enable NVIC
 	NVIC->ISER[1] = 1 << ((PORT4_IRQn)&31);
+
 	//Enable global interrupts
 	__enable_irq();
-
 
 	//infinite loop for embedded system
 	while(1){}
 }
 //
 void SysTick_Handler(void){
-    if(tic++ > 5){
-        P2->OUT ^= BIT2;
-        tic = 0;
+	//100ms * 5 -> .5 seconds 
+    if(tic++ > 5){  //check if .5 seconds have passed
+        P2->OUT ^= BIT2; //toggle led 
+        tic = 0; //reset counter
     }
 }
 void PORT4_IRQHandler(void){
-    if(P4->IFG & BIT0){
-        SysTick->CTRL |= 0x02;
+    if(P4->IFG & BIT0){ //check for BIT0
+        SysTick->CTRL |= 0x02; //enable SysTick Interrupt
     }
-    if(P4->IFG & BIT1){
-        SysTick->CTRL &= ~(0x02);
+    if(P4->IFG & BIT1){ //check for BIT1
+        SysTick->CTRL &= ~(0x02); //disable SysTick Interrupt
     }
-    P4->IFG &= ~(BIT0|BIT1);
+    P4->IFG &= ~(BIT0|BIT1); //clear all bits of interrupt flag
 }
