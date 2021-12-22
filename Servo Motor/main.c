95560f3ce77f0f6168e5611b1caac4e8fd6ed6cd
Jorge Minjares <jorge.minjaresps3@gmail.com>
2021-12-22 15:12:40 -0700

    Added Comments to main.c

--- Servo Motor/main.c
+++ Servo Motor/main.c
@@ -1,28 +1,48 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief This program will demonstrate how to set-up Port 2 as a PWM output to move a Servo Motor
+ * @version 0.1
+ * @date 2021-12-22
+ *
+ * @copyright Copyright (c) 2021
+ *
+ * Positions for the Servo Motor
+ * (-90) = 1ms
+ * (0) = 1.5ms
+ * (90) = 2ms
+ */
 #include "msp.h"
-
 
 /**
  * main.c
  */
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
+	// Enable Port 2 Bit 4 as an output pin
+	P2->DIR |= BIT4;	 // Bit set Port 2: Bit 4
+	P2->OUT |= BIT4;	 // Bit set Port 2: Bit 4
+	P2->SEL0 |= BIT4;	 // Bit set Port 2: Bit 4 to use Special Functions
+	P2->SEL1 &= ~(BIT4); // Bit Clear SEL1
+	// Enable PWM using TimerA
+	// TASSEL_2 = SMCLOCK MC_1 = Up mode
+	TIMER_A0->CTL = TIMER_A_CTL_TASSEL_2 | TIMER_A_CTL_MC_1;
+	TIMER_A0->CCR[0] = 60000 - 1;				// Select period for the SMClock
+	TIMER_A0->CCR[1] = 3000 - 1;				// Select Duty Cycle for the PWM Output
+	TIMER_A0->CCTL[1] = TIMER_A_CCTLN_OUTMOD_7; // Reset/set for PWM
 
-	P2->DIR |= BIT4;
-	P2->OUT |= BIT4;
-	P2->SEL0 |= BIT4;
-	P2->SEL1 &= ~(BIT4);
-
-	TIMER_A0->CTL = TIMER_A_CTL_TASSEL_2|TIMER_A_CTL_MC_1;
-	TIMER_A0->CCR[0] = 60000-1;
-	TIMER_A0->CCR[1] = 3000-1;
-	TIMER_A0->CCTL[1] = TIMER_A_CCTLN_OUTMOD_7;
-
-	while(1){
-	    __delay_cycles(10000);
-	    TIMER_A0->CCR[1] += 1500 - 1;
-	    if(TIMER_A0->CCR[1] > 6000){
-	        TIMER_A0->CCR[1] = 3000-1;
-	    }
+	// TIMER_A0->CCR[1] = 3000-1; set position for servo motor at -90 degrees
+	// TIMER_A0->CCR[1] = 4500-1; set position for servo motor at 0 degrees
+	// TIMER_A0->CCR[1] = 6000-1; set position for servo motor at 90 degrees
+	while (1)
+	{
+		__delay_cycles(10000);
+		TIMER_A0->CCR[1] += 1500 - 1; // add percentage to duty cycle in order to
+		if (TIMER_A0->CCR[1] > 6000)
+		{								 // check if the duty cycle is greater than the period
+			TIMER_A0->CCR[1] = 3000 - 1; // if so, reset the duty cycle to position -90 degrees
+		}
 	}
 }
--- Sine Wave/main.c
+++ Sine Wave/main.c
@@ -1,27 +1,40 @@
+/**
+ * @file main.c
+ * @author Jesus Minjares (https://github.com/jminjares4)
+ * @author Jorge Minjares (https://github.com/JorgeMinjares)
+ * @brief This program will demonstrate how to set-up Port 2 as a PWM output to move a Servo Motor
+ * @version 0.1
+ * @date 2021-12-22
+ *
+ * @copyright Copyright (c) 2021
+ */
 #include "msp.h"
-
-
-uint16_t sineWave[] = {1,12000,24000,36000,48000,60000,48000,36000,24000,12000,1};
+//Sine Wave array, the percentages for PWM 
+uint16_t sineWave[] = {1, 12000, 24000, 36000, 48000, 60000, 48000, 36000, 24000, 12000, 1};
 
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
+	// Enable Port 2: BIT 6 as output port
+	P2->DIR |= BIT6; //Bit Set Port 2: BIT6
+	P2->OUT |= (BIT6);//Bit Set Port 2: BIT6
+	P2->SEL0 |= BIT6; //Bit Set Port 2: BIT6 for special functions
+	P2->SEL1 &= ~(BIT6);//Bit Clear Port 2: BIT6 SEL1
+	
+	//Enable PWM using Timer A
+	//TASSEL_2 = SMCLOCK, MC_1 = UP-mode
+	TIMER_A0->CTL = TIMER_A_CTL_TASSEL_2 | TIMER_A_CTL_MC_1;
+	TIMER_A0->CCR[0] = 60000 - 1; //Select period for SMClock
+	TIMER_A0->CCR[3] = 1 - 1; //Select Duty cycle for PWM Output
+	TIMER_A0->CCTL[3] = TIMER_A_CCTLN_OUTMOD_7; //Reset/set for PWM
 
-	P2->DIR |= BIT6;
-	P2->OUT |= (BIT6);
-	P2->SEL0 |= BIT6;
-	P2->SEL1 &= ~(BIT6);
-
-
-	TIMER_A0->CTL = TIMER_A_CTL_TASSEL_2|TIMER_A_CTL_MC_1;
-	TIMER_A0->CCR[0] = 60000-1;
-	TIMER_A0->CCR[3] = 1-1;
-	TIMER_A0->CCTL[3] = TIMER_A_CCTLN_OUTMOD_7;
-
-	uint8_t i;
-	while(1){
-	   for(i = 0; i < sizeof(sineWave)/sizeof(uint16_t); i++){
-	       __delay_cycles(100000);
-	       TIMER_A0->CCR[3] = sineWave[i]-1;
-	   }
+	uint8_t i; // declare int i for for-loop
+	while (1)
+	{
+		for (i = 0; i < sizeof(sineWave) / sizeof(uint16_t); i++)
+		{
+			__delay_cycles(1000000);
+			TIMER_A0->CCR[3] = sineWave[i] - 1; //using for-loop to switch the duty-cycle for PWM Output
+		}
 	}
+}
