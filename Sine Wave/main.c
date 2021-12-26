a6195180aafa0d4ef9bb776b005ff1d25b35d34e
jminjares4 <jminjares4@miners.utep.edu>
2021-12-26 15:34:28 -0700

    Updated sine wave files

--- Sine Wave/main.c
+++ Sine Wave/main.c
@@ -8,13 +8,16 @@
  *
  * @copyright Copyright (c) 2021
  */
+
 #include "msp.h"
+
 // Sine Wave array, the percentages for PWM
 uint16_t sineWave[] = {1, 12000, 24000, 36000, 48000, 60000, 48000, 36000, 24000, 12000, 1};
 
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
+
 	// Enable Port 2: BIT 6 as output port
 	P2->DIR |= BIT6;	 // Bit Set Port 2: BIT6
 	P2->OUT |= (BIT6);	 // Bit Set Port 2: BIT6
@@ -28,17 +31,13 @@ void main(void)
 	TIMER_A0->CCR[3] = 1 - 1;					// Select Duty cycle for PWM Output
 	TIMER_A0->CCTL[3] = TIMER_A_CCTLN_OUTMOD_7; // Reset/set for PWM
 
-	uint8_t i; // declare int i for for-loop
 	while (1)
 	{
+		uint8_t i; // declare int i for for-loop
 		for (i = 0; i < sizeof(sineWave) / sizeof(uint16_t); i++)
 		{
 			__delay_cycles(1000000);
 			TIMER_A0->CCR[3] = sineWave[i] - 1; // using for-loop to switch the duty-cycle for PWM Output
 		}
 	}
-<<<<<<< HEAD
 }
-== == == =
-}
->>>>>>> 7bfcf041f44c0a9584aa8674b50d99d25df7cb36
